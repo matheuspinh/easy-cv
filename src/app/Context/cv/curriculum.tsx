83803c825305c3ff96632762ff902c08d3e76720
matheus.s.pinheiro@live.com <matheus.s.pinheiro@live.com>
2023-07-05 16:07:54 -0300

    eslint

--- src/app/Context/cv/curriculum.tsx
+++ src/app/Context/cv/curriculum.tsx
@@ -1,24 +1,24 @@
-import {
-  ReactNode,
-  createContext,
-  useCallback,
-  useContext,
-  useState,
-} from 'react'
+// import {
+//   ReactNode,
+//   createContext,
+//   useCallback,
+//   useContext,
+//   useState,
+// } from 'react'
 
-const CurriculumContext = createContext({})
+// const CurriculumContext = createContext({})
 
-export const CurriculumProvider = ({ children }: { children: ReactNode }) => {
-  const [personalDetails, setPersonalDetails] = useState({})
-  const [about, setAbout] = useState('')
-  const [education, setEducation] = useState([])
-  const [skills, setSkills] = useState([])
-  const [experience, setExperience] = useState([])
-  const [certifications, setCertifications] = useState([])
+// export const CurriculumProvider = ({ children }: { children: ReactNode }) => {
+//   const [personalDetails, setPersonalDetails] = useState({})
+//   const [about, setAbout] = useState('')
+//   const [education, setEducation] = useState([])
+//   const [skills, setSkills] = useState([])
+//   const [experience, setExperience] = useState([])
+//   const [certifications, setCertifications] = useState([])
 
-  return (
-    <CurriculumContext.Provider value={{}}>
-      {children}
-    </CurriculumContext.Provider>
-  )
-}
+//   return (
+//     <CurriculumContext.Provider value={{}}>
+//       {children}
+//     </CurriculumContext.Provider>
+//   )
+// }
